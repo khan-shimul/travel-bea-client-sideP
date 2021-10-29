dc4e6f67fb7b2afd41f2f11b61491ac2e9078677
shimulkhan <khanshimul196@gmail.com>
2021-10-30 01:09:07 +0600

    update package title

--- src/components/Home/TourPackage/TourPackage.js
+++ src/components/Home/TourPackage/TourPackage.js
@@ -3,12 +3,11 @@ import React from 'react';
 const TourPackage = () => {
     return (
         <div className="container my-5">
-            <div className="d-flex justify-content-center">
+            <div className="d-flex justify-content-center align-items-center flex-column">
                 <h2 className="fw-bold">Select Package For Your Travel</h2>
-            </div>
-            <div className="d-flex justify-content-center">
                 <h5>Choose Package</h5>
             </div>
+
 
         </div>
     );
