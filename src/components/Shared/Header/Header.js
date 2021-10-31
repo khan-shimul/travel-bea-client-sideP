f49cb32e1291072788e2bf60ac587e06f0cefa65
shimulkhan <khanshimul196@gmail.com>
2021-10-31 17:24:45 +0600

    update header

--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -11,13 +11,14 @@ const Header = () => {
 
     const activeStyle = {
         fontWeight: "bold",
-        // color: "red"
+        color: "#FF7F47"
     }
+
     return (
-        <div>
-            <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
+        <>
+            <Navbar sticky="top" bg="light" variant="light" collapseOnSelect expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand as={NavLink} to="/home">
                         <img
                             src={logo}
                             width="50"
@@ -27,28 +28,32 @@ const Header = () => {
                         />
                     </Navbar.Brand>
                     <Navbar.Toggle />
-                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end tex-white">
-                        <Nav className="me-auto">
-                            <Nav.Link as={NavLink} to="/home"
-                                activeStyle={activeStyle}
-                                className="text-white"
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                        <Nav className="me-auto nav-menu">
+                            <Nav.Link as={HashLink} to="/home#home"
                             >Home</Nav.Link>
                             <Nav.Link as={HashLink} to="/home#tour-package"
                                 activeStyle={activeStyle}
-                                className="text-white"
                             >Tour Package</Nav.Link>
-                            <Nav.Link as={NavLink} to="/home#about"
+                            <Nav.Link as={HashLink} to="/home#about"
                                 activeStyle={activeStyle}
-                                className="text-white"
                             >About</Nav.Link>
                         </Nav>
 
                         {
                             user && <>
-                                <NavLink to="/my-orders">My Orders</NavLink>
+                                <NavLink to="/my-orders" className="text-decoration-none simple-fw"
+                                    activeClassName="selected"
+                                    activeStyle={activeStyle}
+                                >My Booking</NavLink>
                                 <NavDropdown title="Dashboard" id="basic-nav-dropdown">
-                                    <NavDropdown.Item as={NavLink} to="/manage-orders">Manage Orders</NavDropdown.Item>
+                                    <NavDropdown.Item as={NavLink} to="/manage-orders"
+                                        activeClassName="selected"
+                                        activeStyle={activeStyle}
+                                    >Manage Orders</NavDropdown.Item>
                                     <NavDropdown.Item as={NavLink} to="/add-package"
+                                        activeClassName="selected"
+                                        activeStyle={activeStyle}
                                     >Add Package</NavDropdown.Item>
                                 </NavDropdown>
                             </>
@@ -61,14 +66,14 @@ const Header = () => {
                         }
 
                         {
-                            !user ? <Nav.Link as={NavLink} to="/login" className="text-decoration-none text-white "><span><i className="fas fa-sign-in-alt text-primary login-icon"></i></span> Login</Nav.Link> :
-                                <button onClick={logOut}>Logout</button>
+                            !user ? <Nav.Link as={NavLink} to="/login" className="text-dark text-decoration-none fw-bold login">Login</Nav.Link> :
+                                <button onClick={logOut} className="bg-transparent border-0" title="Sign Out"><i className="fas fa-sign-in-alt logout"></i> </button>
                         }
 
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </div>
+        </>
     );
 };
 
