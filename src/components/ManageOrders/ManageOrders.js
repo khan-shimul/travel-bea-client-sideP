c1a07d1ec893c9b02bb53c1eacebc0bfc21a7fb0
shimulkhan <khanshimul196@gmail.com>
2021-10-31 12:55:51 +0600

    updated status

--- src/components/ManageOrders/ManageOrders.js
+++ src/components/ManageOrders/ManageOrders.js
@@ -3,13 +3,14 @@ import { Table } from 'react-bootstrap';
 
 const ManageOrders = () => {
     const [bookedPackages, setBookedPackages] = useState([]);
-    const [isApproved, setIsApproved] = useState(false)
+    const [isApproved, setIsApproved] = useState(false);
 
+    // load booked packs
     useEffect(() => {
         fetch('http://localhost:5000/booked')
             .then(res => res.json())
             .then(data => setBookedPackages(data))
-    }, [isApproved])
+    }, [isApproved]);
 
     // handle delete from all booked packages
     const handleDelete = id => {
@@ -27,7 +28,7 @@ const ManageOrders = () => {
                     }
                 })
         }
-    }
+    };
 
     // handle update
     const handleUpdate = id => {
@@ -49,7 +50,7 @@ const ManageOrders = () => {
                     setIsApproved(true)
                 }
             })
-    }
+    };
 
     return (
         <div>
