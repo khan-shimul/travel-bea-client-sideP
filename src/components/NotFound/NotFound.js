27547428e559e0614b6ef3637495a2fe496b4740
shimulkhan <khanshimul196@gmail.com>
2021-10-31 20:14:47 +0600

    added 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import './NotFound.css';
+import { Button } from 'react-bootstrap';
+import { useHistory } from 'react-router';
+import error from '../../images/404.jpg';
 
 const NotFound = () => {
+    let history = useHistory()
+    const handleBackHome = () => {
+        history.push("/home")
+    }
     return (
-        <div>
-            <h2>404</h2>
+        <div className="overflow-hidden">
+            <div className="row error-container d-flex align-items-center">
+                <div className="col-md-6 px-5">
+                    <h1 className="error-title">Oops! The page you are looking for is not available</h1>
+                    <Button onClick={handleBackHome} className="btn btn-danger mb-3">Back to Home</Button>
+                </div>
+                <div className="col-md-6">
+                    <img className="w-100" src={error} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
